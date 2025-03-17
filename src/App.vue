ccfd0df354a30d6338cddef5f2d6a1bd15fffdb1
elliotabrahamsson <elliotsson@outlook.com>
2025-03-17 19:21:32 +0100

    feat: add Header component and restructure App layout for improved UI

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 import MainContent from "./components/MainContent.vue";
 import Sidebar from "./components/Sidebar.vue";
+import Header from "./components/Header.vue";
 </script>
 
 <template>
-  <div class="flex">
+  <div class="flex h-screen">
     <Sidebar />
-    <MainContent />
+    <div class="flex flex-col flex-1">
+      <Header class="ml-4 space-x-3" />
+      <MainContent class="flex-1" />
+    </div>
   </div>
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex justify-between items-center p-4">
+    <input
+      type="text"
+      placeholder="Search"
+      class="border shadow border-[#F1F0F0] p-1 rounded"
+    />
+    <button
+      class="border shadow border-[#F1F0F0] text-black hover:bg-blue-500 hover:text-white rounded p-1"
+    >
+      Category
+    </button>
+    <button
+      class="border shadow border-[#F1F0F0] text-black hover:bg-blue-500 hover:text-white rounded p-1"
+    >
+      Category
+    </button>
+    <button
+      class="border shadow border-[#F1F0F0] text-black hover:bg-blue-500 hover:text-white rounded p-1"
+    >
+      Advanced
+    </button>
+    <div class="ml-auto space-x-3">
+      <button
+        class="border shadow font-[Roboto] font-bold border-[#F1F0F0] bg-blue-500 text-white rounded p-1"
+      >
+        New Bill
+      </button>
+      <button
+        aria-placeholder="Dots"
+        class="border shadow border-[#F1F0F0] text-black hover:bg-blue-500 hover:text-white rounded p-1"
+      >
+        ...
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
